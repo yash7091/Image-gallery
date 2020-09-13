db465289e904ad1779a7c6ea2ea54991869e4e79
yash7091 <55210722+yash7091@users.noreply.github.com>
2020-09-13 12:26:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-const modal =document.querySelector('.modal')
-const previews = document.querySelectorAll('.gallery img');
-const original = document.querySelector('.full-image');
-const imgText = document.querySelector('.caption');
-previews.forEach(preview=>{
-    preview.addEventListener('click',()=>{
-        modal.classList.add("open");
-
-    })
-})
-modal.addEventListener('click',(e)=>{
-    if(e.target.classList.contains("modal")){
-        modal.classList.remove("open")
-        original.classList.remove("open");
-    }
-})
+  
+function openCarousel() {
+    $(".image-carousel").show();
+  }
+  
+  function closeCarousel() {
+    $(".image-carousel").hide();
+  }
